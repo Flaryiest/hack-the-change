565bf7c45a6bb8d92fd6f03f3db72867ea4edb51
jerry <jerrbearis2cool@gmail.com>
2024-11-09 11:08:48 -0700

    Added database

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from postgrelib import SimpleTable, Database, DataType, Table
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app.route("/login")
-def login():
-    return None
+database = Database(
+    DB_HOST=os.getenv("DB_HOST"),
+    DB_PORT=os.getenv("DB_PORT"),
+    DB_USER=os.getenv("DB_USER"),
+    DB_NAME=os.getenv("DB_NAME"),
+    DB_PASSWORD=os.getenv("DB_PASSWORD"),
+)
+
+table = Table(database, "user_data", [DataType.String("id"), DataType.String("feedback")])
+table.create_table()
+
+
+
