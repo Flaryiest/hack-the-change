943f2fdc0fe381973629e1fac7bf63ebbc5d4a74
jerry <jerrbearis2cool@gmail.com>
2024-11-09 11:46:35 -0700

    Added endpoints + runnable

--- Backend/main.py
+++ Backend/main.py
@@ -1,11 +1,13 @@
-from flask import Flask
-from postgrelib import SimpleTable, Database, DataType, Table
+from flask import Flask, request, jsonify
+from postgrelib import SimpleTable, Database
+from flask_cors import CORS, cross_origin
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 database = Database(
     DB_HOST=os.getenv("DB_HOST"),
@@ -15,8 +17,24 @@ database = Database(
     DB_PASSWORD=os.getenv("DB_PASSWORD"),
 )
 
-table = Table(database, "user_data", [DataType.String("id"), DataType.String("feedback")])
+table = SimpleTable("user_data", database, key="id", item="feedback")
 table.create_table()
+
+@app.route("/submit", methods=["POST"])
+def post():
+    id, feedback = request.json["id"], request.json["id"]
+    if table.get_data(id):
+        table.insert_data(id, {"feedback": feedback})
+    else:
+        return jsonify({"result": False, "info": "This government ID does not exist"})
+
+@app.route("/results", methods=["GET"])
+def results():
+    #do the ML thingy
+    pass
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=3333)
 
 
 
