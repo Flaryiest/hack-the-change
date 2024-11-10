7538f2b424bb32ae52a035186e89f8c9c2f6976c
jerry <jerrbearis2cool@gmail.com>
2024-11-09 17:56:54 -0700

    added cors

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from postgrelib import SimpleTable, Database
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from dotenv import load_dotenv
 from processing import Feedback, Bills
 import os, random, json
@@ -11,6 +11,7 @@ app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+CORS(app, supports_credentials=True)
 database = Database(
     DB_HOST=os.getenv("DB_HOST"),
     DB_PORT=os.getenv("DB_PORT"),
@@ -19,16 +20,26 @@ database = Database(
     DB_PASSWORD=os.getenv("DB_PASSWORD"),
 )
 
-table = SimpleTable("user_data", database, key="id", item="feedback")
+table = SimpleTable("user_data2", database, key="id", item="feedback")
 table.create_table()
+
+bills = SimpleTable("bill_data", database, key="bill", item="feedback")
+table.create_table()
+
+
+cur = database.conn.cursor()
+cur.execute(f"DROP TABLE user_data;")
 
 feedback = Feedback(os.getenv("OPENAI_API_KEY"))
 bill = Bills()
 
 '''
 for i in range(0, 10):
-    table.insert_data("".join([str(random.randint(0, 9)) for i in range(0, 10)]), {"latest_feedback": "", "feedback_history": [], "admin": True, "latest_bill": "", "bill_history": []})
+    table.insert_data("".join([str(random.randint(0, 9)) for i in range(0, 10)]), {"latest_feedback": "", "feedback_history": [], "admin": True})
 '''
+bill.insert_data("Daylight savings time", {"feedback": [], "description": "City proposal to abolish daylight saving."})
+
+
 @app.route("/submit", methods=["POST"])
 def post():
     id, feedback = request.json["id"], request.json["id"]
@@ -42,7 +53,7 @@ def post():
 @app.route("/feedback", methods=["GET"])
 def feedback_endpoint():
     cur = database.conn.cursor()
-    cur.execute(f"SELECT feedback FROM user_data")
+    cur.execute(f"SELECT feedback FROM user_data2")
     rows = cur.fetchall()
 
     feedbacks = []
@@ -57,4 +68,4 @@ def login():
     pass
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=3333)
+    app.run(host="127.0.0.1", port=3333, threaded=True)
