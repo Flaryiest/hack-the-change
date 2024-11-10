501c44c9d5eda91a72879247482f6a2530efa10d
Eric Zuo <ericmzuo1@gmail.com>
2024-11-09 18:56:30 -0700

    minor changes to frontend

--- Frontend/src/Router.jsx
+++ Frontend/src/Router.jsx
@@ -1,12 +1,16 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
 import Layout from "./components/Layout.jsx"
-import DashboardLayout from "./components/Dashboard/DashboardLayout.jsx"
 import Homepage from "./components/Homepage.jsx"
 import Login from "./components/Login.jsx"
+import Solution from "./components/Solution.jsx"
+import Mission from "./components/Mission.jsx"
+
+import DashboardLayout from "./components/Dashboard/DashboardLayout.jsx"
 import Dashboard from "./components/Dashboard/Dashboard.jsx"
 import CreateChange from "./components/Dashboard/CreateChange.jsx"
 import Community from "./components/Dashboard/Community.jsx"
 import MyChanges from "./components/Dashboard/MyChanges.jsx"
+
 import AdminDashboard from "./components/AdminDashboard/AdminDashboard.jsx"
 import AdminLayout from "./components/AdminDashboard/AdminLayout.jsx"
 import CreateBill from "./components/AdminDashboard/CreateBill.jsx"
@@ -19,6 +23,8 @@ const router = createBrowserRouter(
             <Route element={<Layout />}>
                 <Route path="/" element={<Homepage />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/mission" element={<Mission/>} />
+                <Route path="/solution" element={<Solution/>} />
             </Route>
             <Route element={<DashboardLayout />}>
                 <Route path="/dashboard" element={<Dashboard />} />
