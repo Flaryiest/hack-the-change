b5ac3084e6a68121b42a2cab286fea0ccbbfefca
Eric Zuo <ericmzuo1@gmail.com>
2024-11-10 09:00:58 -0700

    login redirect based on perms

--- Frontend/src/components/Dashboard/DashboardLayout.jsx
+++ Frontend/src/components/Dashboard/DashboardLayout.jsx
@@ -2,7 +2,7 @@ import DashboardNavbar from "./DashboardNavbar"
 import { Outlet } from "react-router-dom"
 import { useState, useEffect } from "react"
 export default function DashboardLayout() {
-    const [userInfo, setUserInfo] = useState(null)
+    const [userInfo, setUserInfo] = useState({latest_feedback : "", feedback_history: ['Education is for the weak', 'What daylight is there to save when there is only darkness for our future', 'Books are a vector for misinformation, so they must be censored when needed', 'Animal Dissection is useful for people to know what they are up against in the medical field'], admin: false})
     const [render, setRender] = useState(0)
     function triggerRender() {
         setRender(prevState => prevState += 1)
