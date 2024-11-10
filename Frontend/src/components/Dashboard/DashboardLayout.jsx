84aae20e90a1c02763771fe310111f87ee9a89bc
Eric Zuo <ericmzuo1@gmail.com>
2024-11-10 12:14:50 -0700

    Update DashboardLayout.jsx

--- Frontend/src/components/Dashboard/DashboardLayout.jsx
+++ Frontend/src/components/Dashboard/DashboardLayout.jsx
@@ -6,23 +6,7 @@ export default function DashboardLayout() {
     const [userId, setUserId] = useState(userid)
     const [userInfo, setUserInfo] = useState({latest_feedback : "Education is for the weak", feedback_history: ['Education is for the weak', 'What daylight is there to save when there is only darkness for our future', 'Books are a vector for misinformation, so they must be censored when needed', 'Animal Dissection is useful for people to know what they are up against in the medical field'], admin: false})
     const [feedback, setFeedback] = useState(null)
-    const [bills, setBills] = useState({
-        "Clean Energy Act": {
-          "description": "A bill to promote renewable energy sources and reduce carbon emissions by 50% by 2030.",
-          "feedback": {
-            "User123": "yes",
-            "User456": "no"
-          }
-        },
-        "Affordable Healthcare Act": {
-          "description": "A bill aimed at making healthcare accessible and affordable for all citizens by expanding insurance subsidies.",
-          "feedback": {
-            "User789": "yes",
-            "User101": "yes"
-          }
-        }
-      }
-      )
+    const [bills, setBills] = useState({})
     const [render, setRender] = useState(0)
     function triggerRender() {
         setRender(prevState => prevState += 1)
@@ -57,7 +41,25 @@ export default function DashboardLayout() {
             if (data) {
                 console.log(data, "bills")
                 setBills(data)
-            }   
+            }
+            if (!(data)) {
+                setBills({
+                    "Clean Energy Act": {
+                      "description": "A bill to promote renewable energy sources and reduce carbon emissions by 50% by 2030.",
+                      "feedback": {
+                        "User123": "yes",
+                        "User456": "no"
+                      }
+                    },
+                    "Affordable Healthcare Act": {
+                      "description": "A bill aimed at making healthcare accessible and affordable for all citizens by expanding insurance subsidies.",
+                      "feedback": {
+                        "User789": "yes",
+                        "User101": "yes"
+                      }
+                    }
+                  })
+            }
         }
         
 
