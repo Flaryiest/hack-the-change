0d6fbf9002f86ec4fc80215c5d1ea2b047907a51
Delta2022 <zi.azzr227@gmail.com>
2024-11-10 09:45:42 -0700

    Added comments to main.py

--- Backend/main.py
+++ Backend/main.py
@@ -28,6 +28,12 @@ bills_table.create_table()
 feedback = Feedback(os.getenv("OPENAI_API_KEY"))
 #bill = Bills()
 
+latest_bill_changes = [
+    {"title":"", "new_feedback": ""},
+    {"title":"", "new_feedback": ""},
+    {"title":"", "new_feedback": ""}
+    ]
+
 '''
 for i in range(0, 10):
     user_data_table.insert_data("".join([str(random.randint(0, 9)) for i in range(0, 10)]), {"latest_feedback": "", "feedback_history": [], "admin": True})
@@ -37,19 +43,32 @@ bills_table.insert_data("Daylight savings time", {"feedback": {}, "description":
 
 @app.route("/feedback/submit", methods=["POST"])
 def submit_feedback():
+    # this should be run along with submit bill
+    # changes a user's feedback data
+    # NOTE: the 
     id, feedback = request.json["id"], request.json["feedback"]
 
-    user_data = user_data_table.get_data(id)
+    user_data = user_data_table.get_data(id) # this is a dictionary with the json
+        # section user format
 
+    # return if the user does not exit (user_data would be empty)
     if not user_data:
         return jsonify({"result": False, "info": "This government ID does not exist."})
     
+    # add the old latest feedback to the feedback_history
     user_data["feedback_history"].append(user_data["latest_feedback"])
 
+    # ----- FEEDBACK FORMAT -----
+    # {"feedback_history": [old -----> new]
+
+    # remove the oldest feedback when the length is greater than 10
     if len(user_data["feedback_history"]) > 10:
         user_data["feedback_history"].pop(0)
 
+    # set the feedback to the lastest_feedback
     user_data["latest_feedback"] = feedback
+
+    # change the database
     user_data_table.insert_data(id, {"latestfeedback": feedback})
 
     return jsonify({"result": True, "info": "Success!"})
@@ -57,6 +76,13 @@ def submit_feedback():
 
 @app.route("/bill/submit", methods=["POST"])
 def submit_bill():
+    # submits a bill's feedback into the database
+    # to create a bill, use add_bill
+    # INPUT (roughly)
+        # cookie data with id
+        # {"feedback": ..., "bill": ...}
+    # OUTPUT
+        # {"result": True/False}
     id, feedback, bill = request.cookies.get('id'), request.json["feedback"], request.json["bill"]
 
     user_data = user_data_table.get_data(id)
@@ -80,11 +106,16 @@ def submit_bill():
 
 @app.route("/get_cookie", methods=["POST"])
 def get_cookies():
-    id = request.json["id"]
+    # gets the id from the user and returns a cookie for them to use
+    # INPUT
+        # {"id": ...}
+    # OUTPUT
+        # {"success": True/False}
+    user_id = request.json["id"]
 
-    if user_data_table.get_data(id):
+    if user_data_table.get_data(user_id):
         response = jsonify({"success": True})
-        response.set_cookie('id', id)
+        response.set_cookie('id', user_id)
     else:
         response = jsonify({"success": False})
         
@@ -92,6 +123,13 @@ def get_cookies():
 
 @app.route("/user_data", methods=["GET"])
 def get_user_data():
+    # gets the user data based on their cookie id
+    # INPUT
+        # cookie id
+    # OUTPUT
+        # {"success": True, "data": {...}}
+        # OR
+        # {"success": False}
     id = request.cookies.get('id')
     user_data = user_data_table.get_data(id)
     
@@ -104,9 +142,10 @@ def get_user_data():
 
 @app.route("/result/feedback", methods=["GET"])
 def feedback_results():
+    # outputs the feedback to the user
     cur = database.conn.cursor()
     cur.execute(f"SELECT feedback FROM user_data2")
-    rows = cur.fetchall()
+    rows = cur.fetchall() # rows is a list of tuples? each tuple stores a uesr
 
     feedbacks = []
 
@@ -118,9 +157,10 @@ def feedback_results():
 
 @app.route("/result/bills", methods=["GET"])
 def bills_result():
+
     cur = database.conn.cursor()
     cur.execute("SELECT bill, feedback FROM bill_data")
-    rows = cur.fetchall()
+    rows = cur.fetchall() # rows is now a list of tuples?
 
     result = {row[0]: row[1] for row in rows}
     return jsonify(result)
@@ -135,7 +175,6 @@ def add_bill():
     # OUTPUT
         # {"success": True/False}
 
-
     user_id = request.cookies.get('id')
     user_data = user_data_table.get_data(user_id)
 
@@ -147,6 +186,10 @@ def add_bill():
     else:
         response = jsonify({"success": False, "info": "you are not an admin"})
     return response
+
+@app.route("/latest", methods=["GET"])
+def latest():
+    # gets the last three latest database edits
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, threaded=True)
